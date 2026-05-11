94282524dbdfdce0eefbbd466bf40159041bc8ff
Amir Anwar <amiyer1803@gmail.com>
2026-05-11 12:05:26 +0800

    11052025-fill-database-credentials

--- server/server.js
+++ server/server.js
@@ -32,8 +32,8 @@ if (isProd) {
 const pool = mysql.createPool({
   host:     process.env.DB_HOST     || 'localhost',
   port:     parseInt(process.env.DB_PORT || '3306'),
-  user:     process.env.DB_USER     || 'root',
-  password: process.env.DB_PASSWORD || '',
+  user:     process.env.DB_USER     || 'admin',
+  password: process.env.DB_PASSWORD || '@dminPhn17',
   database: process.env.DB_NAME     || 'hrdc_training',
   waitForConnections: true,
   connectionLimit:    10,
